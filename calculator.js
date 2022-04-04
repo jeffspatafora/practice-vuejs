142561b0abcff95d033d85650e220b33745e591c
jeffspatafora <jeffspatafora@gmail.com>
2022-04-03 18:53:31 -0700

    adds calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,8 +1,21 @@
+/* global Vue */
+
 var App = {
   data: function () {
     return {
       message: "Hello Vue!",
+      number1: 0,
+      number2: 0,
+      number3: 0,
+      sum: 0,
     };
+  },
+  methods: {
+    computeSum: function () {
+      console.log("adding the numbers...");
+      console.log(parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3));
+      this.sum = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+    },
   },
 };
 
