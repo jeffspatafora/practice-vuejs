cb277a7e73a5e9c7d52b97e3e4fbf2ba46a76d8c
jeffspatafora <jeffspatafora@gmail.com>
2022-04-03 17:28:55 -0700

    adds some test data return elements(name, city), adds v-on:click for changing name

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var App = {
   data: function () {
     return {
       message: "Hello from JavaScript!",
+      name: "Jeff",
+      city: "Seattle",
     };
+  },
+  methods: {
+    changeName: function () {
+      console.log("changing the name to Katie");
+      this.name = "Katie";
+      console.log(this.name);
+    },
   },
 };
 
